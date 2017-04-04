1bbde963e65cd6181ff7bcd374796a11798ea6b5
Xu <sai.xu@sap.com>
2017-04-03 20:59:51 -0400

    assign3

--- assignment4.js
+++ assignment4.js
@@ -14,4 +14,21 @@
 (function() {
   // Magic!
   console.log('Keepin\'n it clean with an external script!');
+  var arr;
+  $.get('http://www.mattbowytz.com/simple_api.json?data=programming', function(d){
+  		arr= d.data;			
+  }, 'json');
+  $(".flexsearch-input").bind("keyup", function(){
+  		var input= $(this);
+  		var text= input.val();
+  		var html= '';
+  		if(text.length!= 0){
+  			for(var i= 0; i< arr.length; i++){
+  				if(arr[i].substring(0, text.length)== text){
+  					html+= '<li><a href= https://www.google.com/#q='+arr[i]+'*&>'+ arr[i]+ '</a></li>';
+  				}
+  			}
+  		}
+  		$("#result").html(html);
+  });
 })();
